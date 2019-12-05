ce1643f65017c7fb7bf793391d9d33df3a76f6ac
Monika Daryani <monikadaryani10@gmail.com>
2019-12-05 22:57:55 +0000

    form questions done for new code

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -39,7 +39,7 @@ class QuestionsController < ApplicationController
 
     respond_to do |format|
       if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
+        format.html { redirect_to @question, notice: 'Response was successfully created.' }
         format.json { render :show, status: :created, location: @question }
       else
         format.html { render :new }
