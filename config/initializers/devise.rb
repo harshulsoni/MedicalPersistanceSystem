6e2c1fd8a53fd3da5c03e0bf510b3a0034e48b0a
Harshul Soni <harshulsoni95@gmail.com>
2019-12-08 23:26:38 +0000

    setup sendgrid

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,8 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  #config.mailer_sender = 'harshul@medicalpersistancesystem.com'
-  config.mailer_sender = ENV['GMAIL_USERNAME']
+  config.mailer_sender = 'harshul@dry-temple-11841.herokuapp.com'
   config.reconfirmable = false # unless you are using reconfirmable!
 
   # Configure the class responsible to send e-mails.
